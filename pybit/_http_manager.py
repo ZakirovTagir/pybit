18f281a40d03dd308bb489c55977369271e08be4
ZakirovTagir <tagir1987@mail.ru>
2026-07-06 21:43:11 +0300

    Fix defaultdict[dict] for Python 3.8

--- pybit/_http_manager.py
+++ pybit/_http_manager.py
@@ -106,7 +106,7 @@ class _V5HTTPManager:
     timeout: int = field(default=10)
     recv_window: bool = field(default=5000)
     force_retry: bool = field(default=False)
-    retry_codes: defaultdict[dict] = field(default_factory=dict)
+    retry_codes: defaultdict = field(default_factory=dict)
     ignore_codes: set = field(default_factory=set)
     max_retries: bool = field(default=3)
     retry_delay: bool = field(default=3)
